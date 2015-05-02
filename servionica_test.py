f9fbaed2a85eb6ce12153b5bf697c2807bbdd655
Владимир Островерхов <Xrono@MacBook-Pro-Vladimir.local>
2015-05-02 03:20:11 +0300

    add dictionary
    all information write to file
    
    all information write to file, used new dictionary witch remember order of arguments in dictionary

--- servionica_test.py
+++ servionica_test.py
@@ -1,7 +1,8 @@
 import os
-import mimetypes
+from collections import OrderedDict
 import argparse
 import struct
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--path_to_file", type=str,
@@ -16,35 +17,6 @@ args = parser.parse_args()
 
 PATH_TO_INPUT_CATOLOGUE = args.path_to_catologue
 PATH_TO_OUTPUT_FILE = args.path_to_file
-
-# PATH_TO_INPUT_CATOLOGUE = "/Users/Xrono/Desktop/python/new"
-# PATH_TO_OUTPUT_FILE = "output.txt"
-
-
-def get_qcow_file_dict(qcowfile):
-    file_dictionary = {}
-    file_name = qcowfile
-    file_dictionary['filename'] = file_name
-    size = os.path.getsize(qcowfile)
-    file_dictionary['size'] = size
-    with open(qcowfile, "rb") as binary_file:
-        virtual_size = get_info(binary_file, 24, 8, '>Q')
-        file_dictionary['virtualsize'] = virtual_size
-        is_bf = check_bf_exist(binary_file)
-        if is_bf:
-            backing_file_name = get_bf_name(binary_file, is_bf)
-            file_dictionary['backing_file'] = backing_file_name
-        is_snapshots = check_snapshots_exist(binary_file)
-        if is_snapshots:
-            file_dictionary['snapshots'] = []
-            count_of_snapshots = is_snapshots
-            snapshots_offset = get_info(
-                binary_file, 64, 8, '>Q')
-            while count_of_snapshots > 0:
-                snapshot_dict, snapshots_offset = snapshots_info(binary_file, snapshots_offset)
-                file_dictionary['snapshots'].append(snapshot_dict)
-                count_of_snapshots -= 1
-    return file_dictionary
 
 
 def get_info(b_fileqcow, start_position, cout_of_reading_bytes, unpack_format):
@@ -80,59 +52,91 @@ def check_snapshots_exist(b_fileqcow):
 
 
 def walk_through_tree_of_catalogue_qcow2(in_cotalogue, out_file):
+    # check path to cotalogue
     if os.path.exists(in_cotalogue):
+        # check path to out_file
         if os.path.exists(str(os.path.split(
                 os.path.abspath(out_file))[0]) + '/'):
-            file_clean = open(out_file, 'w')
-            file_clean.close()
+            list_of_dict = []
             for d, dirs, files in os.walk(PATH_TO_INPUT_CATOLOGUE):
                 for f in files:
                     with open(os.path.join(d, f), "rb") as binary_file:
+                        # check qcow2 file
                         byte = binary_file.read(3)
                         if byte == "QFI":
                             fileqcow = os.path.join(d, f)
-                            qcow_file_dict = get_qcow_file_dict(fileqcow)
-                            print (qcow_file_dict)
+                            list_of_dict.append(get_qcow_file_dict(fileqcow))
         else:
             print "Wrong path to the file"
     else:
         print "No such directory"
+    write_information_to_output_file(out_file, list_of_dict)
 
 
-def snapshots_info(b_fileqcow, snapshots_offset):
+def snapshots_info(b_fileqcow, snap_offset):
 
-    offset = get_info(b_fileqcow, snapshots_offset, 8, '>Q')
-    len_id = get_info(b_fileqcow, snapshots_offset + 12, 2, '>H')
-    len_name = get_info(b_fileqcow, snapshots_offset + 14, 2, '>H')
-    snapshots_size = get_info(b_fileqcow, snapshots_offset + 32, 4, '>I')
-    extra_data_size = get_info(b_fileqcow, snapshots_offset + 36, 4, '>I')
+    offset = get_info(b_fileqcow, snap_offset, 8, '>Q')
+    len_id = get_info(b_fileqcow, snap_offset + 12, 2, '>H')
+    len_name = get_info(b_fileqcow, snap_offset + 14, 2, '>H')
+    snapshots_size = get_info(b_fileqcow, snap_offset + 32, 4, '>I')
+    extra_data_size = get_info(b_fileqcow, snap_offset + 36, 4, '>I')
     snapshots_id = get_info(
                             b_fileqcow,
-                            snapshots_offset + 40 + extra_data_size,
+                            snap_offset + 40 + extra_data_size,
                             len_id, str(len_id) + 's'
                             )
     snapshots_name = get_info(
                                 b_fileqcow,
-                                snapshots_offset + 40 + extra_data_size + len_id,
+                                snap_offset + 40 + extra_data_size + len_id,
                                 len_name, str(len_name) + 's'
                                 )
-    len_of_ss = snapshots_offset + 40 + extra_data_size + len_id + len_name
+    len_of_ss = snap_offset + 40 + extra_data_size + len_id + len_name
+    # check if offset not multiply 8 and plus missing bits
     if len_of_ss % 8 != 0:
         len_of_ss = len_of_ss + (8 - (len_of_ss % 8))
-    snap_dict = {
-                 'id': snapshots_id,
-                 'name': snapshots_name,
-                 'virtual_size': snapshots_size
-                }
+    # dictionary, which stores the input order of arguments
+    snap_dict = OrderedDict()
+    snap_dict['id'] = snapshots_id
+    snap_dict['name'] = snapshots_name
+    snap_dict['virtualsize'] = snapshots_size
     return (snap_dict, len_of_ss)
 
 
-def write_information_to_output_file(file_to):
+def get_qcow_file_dict(qcowfile):
+    # dictionary, which stores the input order of arguments
+    file_dictionary = OrderedDict()
+    file_dictionary['filename'] = qcowfile
+    size = os.path.getsize(qcowfile)
+    file_dictionary['size'] = size
+    with open(qcowfile, "rb") as binary_file:
+        virtual_size = get_info(binary_file, 24, 8, '>Q')
+        file_dictionary['virtualsize'] = virtual_size
+        # check availability of backing file
+        is_bf = check_bf_exist(binary_file)
+        if is_bf:
+            backing_file_name = get_bf_name(binary_file, is_bf)
+            file_dictionary['backing_file'] = backing_file_name
+        # check availability of snapshots
+        is_snapshots = check_snapshots_exist(binary_file)
+        if is_snapshots:
+            file_dictionary['snapshots'] = []
+            count_of_snapshots = is_snapshots
+            snap_offset = get_info(
+                binary_file, 64, 8, '>Q')
+            # pass on all snapshots
+            while count_of_snapshots > 0:
+                snapshot_dict, snap_offset = snapshots_info(
+                                            binary_file, snap_offset)
+                file_dictionary['snapshots'].append(snapshot_dict)
+                count_of_snapshots -= 1
+    return file_dictionary
 
-    with open(file_from) as file_open_temp:
-        content = file_open_temp.readlines()
-    with open(file_to, 'a') as file_out_temp:
-        file_out_temp.write("".join(content))
-        file_out_temp.write("".join('\n'))
+
+def write_information_to_output_file(file_to, dictionary):
+    # write information about qcow2 file
+    with open(file_to, 'w') as file_out_temp:
+        json.dump(dictionary, file_out_temp, indent=2, ensure_ascii=False)
+
+
 walk_through_tree_of_catalogue_qcow2(
     PATH_TO_INPUT_CATOLOGUE, PATH_TO_OUTPUT_FILE)
